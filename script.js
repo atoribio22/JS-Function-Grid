4c2ecbba4fb56a9626820391774efb346785e73d
Ashley Toribio <atoribio@aclanyc.com>
2017-02-27 13:21:35 +0000

    Made Some Changes

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //Write your function calls below this line
+// Four Corners
+setColor(0, 0, "blue");
+setColor(9, 9, "blue");
+setColor(9, 0, "blue");
+setColor(0, 9, "blue");
+
+
+//Smiley Face
+setColor(2, 3, "red");
+setColor(2, 6, "red");
+setColor(5, 3, "red");
+setColor(6, 4, "red");
+setColor(6, 5, "red");
+setColor(5, 6, "red");
+
+//The letter S
